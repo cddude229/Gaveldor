5fde7cf8532da3f0e8351c86bcc119e03f09f2be
Chris Dessonville <cdville@mit.edu>
2012-09-23 18:45:46 -0400

    Adding state class.

---
+++ src/state.py
@@ -0,0 +1,20 @@
+class State:
+    pieces = [[]]
+    terrain = [[]]
+    gameStatus = 0 # 0 = playing, 1,2 = who won, 3 = stalemate
+    currentTurn = 1 # Whose turn is it?
+    def getPiece(x, y):
+        if x < 0 or y < 0: # Make sure in bounds
+            return None
+        if x >= len(pieces) or y >= len(pieces[0]): # Make sure in bounds, again
+            return None
+
+        return pieces[x][y]
+
+    def getTerrain(x, y):
+        if x < 0 or y < 0:
+            return "water"
+        if x >= len(terrain) or y >= len(terrain[0]):
+            return "water"
+
+        return terrain[x][y]
