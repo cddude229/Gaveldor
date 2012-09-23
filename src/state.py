3d7616e01b8afc9820fa9bf5c0d449134e6e4c4f
Chris Dessonville <cdville@mit.edu>
2012-09-23 18:47:22 -0400

    Spacing, commenting for state.py

--- src/state.py
+++ src/state.py
@@ -3,17 +3,22 @@ class State:
     terrain = [[]]
     gameStatus = 0 # 0 = playing, 1,2 = who won, 3 = stalemate
     currentTurn = 1 # Whose turn is it?
+
     def getPiece(x, y):
+    	# Determine which piece (if any) exists at (x, y)
         if x < 0 or y < 0: # Make sure in bounds
             return None
+
         if x >= len(pieces) or y >= len(pieces[0]): # Make sure in bounds, again
             return None
 
         return pieces[x][y]
 
     def getTerrain(x, y):
+    	# Determine which terrain type (if any) exists at (x, y)
         if x < 0 or y < 0:
             return "water"
+
         if x >= len(terrain) or y >= len(terrain[0]):
             return "water"
 
