8450055821b5e154ac5b1daa265f2dbb0a7ffeec
unknown <Genephage@HappyFunTime.(none)>
2012-09-23 20:19:23 -0400

    Player class added

---
+++ src/player.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Player:
+    def __init__(self, id):
+        self.id = id
+        self.pieces = []
+    def hasPiecesLeft(self):
+        pass
+    def getPieces(self):
+        return self.pieces
