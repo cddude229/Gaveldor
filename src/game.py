8d38399037eece4b194b510af1ca9cff15b134f3
Benjamin Greenberg <bengreenberg@Benjamin-Greenbergs-MacBook-Pro.local>
2012-09-23 20:54:03 -0400

    gridding done

--- src/game.py
+++ src/game.py
@@ -30,9 +30,9 @@ spaces = pygame.sprite.RenderPlain()
 for i in range(x):
     for j in range(y):
         if i%2==0:
-            s=Space(i*screenw/x-i*screenw/x/4,j*screenh/y,i,j)#-j*screenh/y/3,i,j)
+            s=Space(i*screenw/x-i*screenw/x/4,j*screenh/y,i,j*2)#-j*screenh/y/3,i,j)
         else:
-            s=Space(i*screenw/x-i*screenw/x/4,int((j+.5)*screenh/y),i,j)#-j*screenh/y/3),i,j+1)
+            s=Space(i*screenw/x-i*screenw/x/4,int((j+.5)*screenh/y),i,j*2+1)#-j*screenh/y/3),i,j+1)
         spaces.add(s)
 
 #finds which spaces are occupied
@@ -53,8 +53,20 @@ while done==False:
                 gameover=True
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x1,y1=pygame.mouse.get_pos()
-                start = (x1/(screenw/x),y1/(screenh/y))
-                print start
+                startx = x1/(3*screenw/x/4)
+                if startx%2==0:
+                    start=(startx,y1/(screenh/y)*2)
+                else:
+                    start=(startx,(y1-screenh/y/2)/(screenh/y)*2+1)
+            if event.type == pygame.MOUSEBUTTONUP:
+                x2,y2=pygame.mouse.get_pos()
+                endx = x2/(3*screenw/x/4)
+                if endx%2==0:
+                    end=(endx,y2/(screenh/y)*2)
+                else:
+                    end=(endx,(y2-screenh/y/2)/(screenh/y)*2+1)
+                print end
+            
         for i in spaces:
             if not gs.getPiece(i.x,i.y)==None:
                 i.update(gs.getPiece(i.x,i.y))
