707363a03357eae4fc656a1ef8656338291dad80
Chris Dessonville <cdville@mit.edu>
2012-09-23 21:19:38 -0400

    Making a default piece in spot 0, 0

--- src/player.py
+++ src/player.py
@@ -3,7 +3,9 @@ import pygame
 class Player:
     def __init__(self, id):
         self.id = id
-        self.pieces = []
+        self.pieces = [
+            Infantry(0, 0, 3)
+        ]
     def hasPiecesLeft(self):
         return len(pieces) > 0
     def getPieces(self):
